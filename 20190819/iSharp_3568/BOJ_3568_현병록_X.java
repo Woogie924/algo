488673f6d1be9893e8c920bc1819a71f39782957
현병록 <hyunbyungrok@gmail.com>
2019-08-18 07:47:15 +0000

    [틀렸습니다]

---
+++ 20190819/iSharp_3568/BOJ_3568_현병록_X.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_3568_현병록_X {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		//입력받고 첫 토큰은 기존 변수형일테니 start에 저장해둔다.
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String start = st.nextToken();
+		String token;
+
+		while(st.hasMoreTokens()) {
+			token = st.nextToken();
+			//뒤에서부터 읽어서 붙일거라서 배열을 반대로 변경하여준다.
+			token = token.replace("[]", "][");
+			bw.write(start);
+			for(int i =token.length()-2; i>=0; i--) {
+				char c = token.charAt(i);
+				if(c=='['||c==']'||c=='&'||c=='*')
+					bw.append(token.charAt(i));
+			}
+			bw.append(" " + token.charAt(0) + ";");
+			bw.newLine();
+		}
+
+		bw.flush();
+	}
+}

5fd73b2d2378a731d836e2823c7e049be4e36f9e
박정호 <qkrwjdgh0904@naver.com>
2019-10-10 08:14:50 +0000

    게리멘더링 이제된당..

--- 20191014/게리맨더링/BOJ_17471_박정호_O.java
+++ 20191014/게리맨더링/BOJ_17471_박정호_O.java
@@ -5,12 +5,13 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class BOJ_게리맨더링_17471_박정호_X {
+public class BOJ_게리맨더링_17471_박정호_O {
 	static int N;
 	static int[] people;
 	static boolean[] visit;
 	static int[][] map;
 	static int answer = Integer.MAX_VALUE;
+	static boolean flag = false; 
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,48 +30,43 @@ public class BOJ_게리맨더링_17471_박정호_X {
 				map[i][Integer.parseInt(s[j])] = 1;
 			}
 		}
-//		print();
 		ArrayList<Integer> list = new ArrayList<>();
 		for(int i=1; i<=N/2; ++i)
 			go(1, i, list);
-		if(answer==Integer.MAX_VALUE)
+		if(!flag)
 			System.out.println(-1);
 		else
 			System.out.println(answer);
 	}
 
 	private static void solution(ArrayList<Integer> list1, ArrayList<Integer> list2) {
-//		System.out.println(list1+ " "+list2);
-		// 모두 연결되었는지 확인
-		if(!isLinked(list1, list2))
+		// 두 지역구로 나눌 수 있는지 확인
+		if(isLinked(list1)!=1 || isLinked(list2) != 1)
 			return;
-		
+		flag = true;
 		// 인원수 비교
 		int temp = 0;
 		for(int i=0; i<list1.size(); ++i)
 			temp += people[list1.get(i)];
 		for(int i=0; i<list2.size(); ++i)
 			temp -= people[list2.get(i)];
-		temp = Math.abs(temp);
-		answer = Math.min(answer, temp);
-//		System.out.println(list1+" "+list2+" "+temp);
+		answer = Math.min(answer, Math.abs(temp));
 	}
 
-	private static boolean isLinked(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+	private static int isLinked(ArrayList<Integer> list) {
 		boolean[] check = new boolean[N+1];
 		int cnt = 0;
-		for(int i=0; i<list1.size(); ++i) {
-			int x = list1.get(i);
+		for(int i=0; i<list.size(); ++i) {
+			int x = list.get(i);
 			if(check[x])
 				continue;
 			 Queue<Integer> q = new LinkedList<Integer>();
 			 q.add(x);
 			 while(!q.isEmpty()) {
 				 int tx = q.poll();
-				 if(check[x])
-						continue;
+				 check[tx] = true;
 				 for(int j=1; j<=N; ++j) {
-					 if(map[tx][j]==1 && list1.contains(j)) {
+					 if(map[tx][j]==1 && list.contains(j) && !check[j]) {
 						 check[j]=true;
 						 q.add(j);
 					 }
@@ -78,29 +74,7 @@ public class BOJ_게리맨더링_17471_박정호_X {
 			 }
 			 cnt++;
 		}
-		check = new boolean[N+1];
-		for(int i=0; i<list2.size(); ++i) {
-			int x = list2.get(i);
-			if(check[x])
-				continue;
-			 Queue<Integer> q = new LinkedList<Integer>();
-			 q.add(x);
-			 while(!q.isEmpty()) {
-				 int tx = q.poll();
-				 if(check[x])
-						continue;
-				 for(int j=1; j<=N; ++j) {
-					 if(map[tx][j]==1 && list2.contains(j)) {
-						 check[j]=true;
-						 q.add(j);
-					 }
-				 }
-			 }
-			 cnt++;
-		}
-		if(cnt==2)
-			return true;
-		return false;
+		return cnt;
 	}
 
 	private static void go(int n, int size, ArrayList<Integer> list) {
@@ -111,6 +85,7 @@ public class BOJ_게리맨더링_17471_박정호_X {
 					continue;
 				list2.add(i);
 			}
+//			System.out.println(list+" "+list2);
 			solution(list, list2);
 			return;
 		}

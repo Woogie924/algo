2d10eefd5ea408f9a434505c1fd606d1671feb1f
황영은 <ktt7566@naver.com>
2019-12-15 16:24:16 +0000

    Update 프로그래머스_60063_황영은_O.java

--- 20191216/프로그래머스_블록이동하기/프로그래머스_60063_황영은_O.java
+++ 20191216/프로그래머스_블록이동하기/프로그래머스_60063_황영은_O.java
@@ -6,10 +6,10 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class P_60063_블록이동하기_X {
+public class P_60063_블록이동하기 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = 5;
+		int N = Integer.parseInt(br.readLine());
 		int[][] board = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,49 +25,73 @@ public class P_60063_블록이동하기_X {
 		int[][] right_h = { { -1, -1 }, { 1, -1 }, { -1, 1 }, { 1, 1 } }; // 90도 방향
 		int[][] h = { { -1, 0 }, { 1, 0 } }; // 가로일 때
 
-		int[][] right_v = { { 1, -1 }, { 1, 1 }, { -1, -1 }, { -1, 1 } };
+		int[][] right_v = { { -1, -1 }, { -1, 1 }, { 1, -1 }, { 1, 1 } };
 		int[][] v = { { 0, -1 }, { 0, 1 } }; // 세로일 때
 
 		int[][] dir = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } }; // 상하좌우
 
 		public int solution(int[][] board) {
-//			for (int i = 0; i < board.length; i++) {
-//				System.out.println(Arrays.toString(board[i]));
-//			}
+			int answer = 0;
+//         for (int i = 0; i < board.length; i++) {
+//            System.out.println(Arrays.toString(board[i]));
+//         }
 
 			int N = board.length;
 			Queue<Point> q = new LinkedList<>();
 			q.add(new Point(0, 0, 0, 1, 0)); // 시작 지점
 			int[][][] visited = new int[N][N][2]; // 0: 가로, 1: 세로
-
-			while (!q.isEmpty()) {
+			visited[0][0][0] = -1;
+			exit : while (!q.isEmpty()) {
 				Point temp = q.poll();
-
+				
+//				System.out.println("(" + temp.x1 + ", "  + temp.y1 + ") (" + temp.x2 + ", " + temp.y2 + ")");
 				for (int i = 0; i < 4; i++) {
 					int dx1 = temp.x1 + dir[i][0];
 					int dy1 = temp.y1 + dir[i][1];
 					int dx2 = temp.x2 + dir[i][0];
 					int dy2 = temp.y2 + dir[i][1];
-
-					if (dx1 < 0 || dy1 < 0 || dx2 < 0 || dy2 < 0 || dx2 < 0 || dy2 < 0) continue;
-					if (dx1 >= N || dy1 >= N || dx2 >= N || dy2 >= N || dx2 >= N || dy2 >= N) continue;
 					
 					int d = 1;
-					if(dy1 != dy2) d = 0;
+					if (dy1 != dy2) d = 0;
 					
-					if (visited[dx1][dy1][d] != 0 && visited[dx1][dy1][d] <= temp.cnt + 1) continue;
-					if (board[dx1][dy1] == 1 || board[dx2][dy2] == 1) continue;
-					if ((dx1 == N - 1 && dy1 == N - 1) || (dx2 == N - 1 && dy2 == N - 1)) {
-						return temp.cnt + 1;
+					if(dy1 > dy2 || dx1 > dx2) {
+						int t = dx1;
+						dx1 = dx2;
+						dx2 = t;
+						t = dy1;
+						dy1 = dy2;
+						dy2 = t;
 					}
+					
+					if (dx1 < 0 || dy1 < 0 || dx2 < 0 || dy2 < 0 || dx2 < 0 || dy2 < 0) continue;
+					if (dx1 >= N || dy1 >= N || dx2 >= N || dy2 >= N || dx2 >= N || dy2 >= N) continue;
+					if (board[dx1][dy1] == 1 || board[dx2][dy2] == 1) continue;
+					
+//					System.out.print(visited[dx1][dy1][d] + " , " + (temp.cnt + 1) + " || ");
+//					System.out.print("1 : " + visited[dx1][dy1][d] + "  || ");
+					Point p = new Point(dx1, dy1, dx2, dy2, temp.cnt + 1);
+//					System.out.print(p + " | " + (d == 0 ? "가로" : "세로"));
+					
+					if (visited[dx1][dy1][d] == -1 || (visited[dx1][dy1][d] != 0 && visited[dx1][dy1][d] <= temp.cnt + 1)) {
+//						System.out.println(" -> X");
+						continue;
+					}
+					if ((dx1 == N - 1 && dy1 == N - 1) || (dx2 == N - 1 && dy2 == N - 1)) {
+						answer = temp.cnt + 1;
+						break exit;
+					}
+					
+//					System.out.println(" -> O");
+					
 					visited[dx1][dy1][d] = temp.cnt + 1;
-					q.add(new Point(dx1, dy1, dx2, dy2, temp.cnt + 1));
+					
+					q.add(p);
 				}
 
 				for (int i = 0; i < 4; i++) {
 					int dx1 = 0, dx2 = 0, dy1 = 0, dy2 = 0; // dx1, dy1가 고정
 					int cx = 0, cy = 0;
-
+					
 					if (temp.x1 == temp.x2) {
 						if (i < 2) {
 							dx1 = temp.x1;
@@ -102,44 +126,56 @@ public class P_60063_블록이동하기_X {
 						}
 					}
 
+					int d = 1;
+					if (dy1 != dy2) d = 0;
+					
+					if(dy1 > dy2 || dx1 > dx2) {
+						int t = dx1;
+						dx1 = dx2;
+						dx2 = t;
+						t = dy1;
+						dy1 = dy2;
+						dy2 = t;
+					}
+//					System.out.printf("%d, %d, %d, %d\n", dx1, dy1, dx2, dy2);
 					if (dx1 < 0 || dy1 < 0 || dx2 < 0 || dy2 < 0 || dx2 < 0 || dy2 < 0) continue;
 					if (dx1 >= N || dy1 >= N || dx2 >= N || dy2 >= N || dx2 >= N || dy2 >= N) continue;
 					if (board[dx1][dy1] == 1 || board[dx2][dy2] == 1 || board[cx][cy] == 1) continue;
 					
-					int d = 1;
-					if(dy1 != dy2) d = 0;
+//					System.out.print(visited[dx1][dy1][d] + " , " + (temp.cnt + 1) + " || ");
+//					System.out.print("2 : " + visited[dx1][dy1][d] + "  || ");
+					Point p = new Point(dx1, dy1, dx2, dy2, temp.cnt + 1);
+//					System.out.print(p + " | " + (d == 0 ? "가로" : "세로"));
 					
-					if (visited[dx1][dy1][d] != 0 && visited[dx1][dy1][d] <= temp.cnt + 1) continue;
+					if (visited[dx1][dy1][d] == -1 || (visited[dx1][dy1][d] != 0 && visited[dx1][dy1][d] <= temp.cnt + 1)) {
+//						System.out.println(" -> X");
+						continue;
+					}
 					if ((dx1 == N - 1 && dy1 == N - 1) || (dx2 == N - 1 && dy2 == N - 1)) {
-						return temp.cnt + 1;
+						answer = temp.cnt + 1;
+						break exit;
 					}
 					
+//					System.out.println(" -> O");
 					visited[dx1][dy1][d] = temp.cnt + 1;
-					q.add(new Point(dx1, dy1, dx2, dy2, temp.cnt + 1));
+					q.add(p);
+
 				}
-
+//				System.out.println();
 			}
-
-			return 0;
+			return answer;
 		}
 	}
-
+	
 	public static class Point {
 		int x1, y1, x2, y2, cnt;
 
 		public Point(int x1, int y1, int x2, int y2, int cnt) {
-			if(y1 < y2) {
-				this.x1 = x1;
-				this.y1 = y1;
-				this.x2 = x2;
-				this.y2 = y2;
-			} else {
-				this.x1 = x2;
-				this.y1 = y2;
-				this.x2 = x1;
-				this.y2 = y1;
-			}
-			
+			super();
+			this.x1 = x1;
+			this.y1 = y1;
+			this.x2 = x2;
+			this.y2 = y2;
 			this.cnt = cnt;
 		}
 
